da1aa998456d88f68c11f2a03754f5e4dc1c6dd8
ognjenvujovic04 <143551253+ognjenvujovic04@users.noreply.github.com>
2025-02-18 00:24:34 +0100

    Add get_q_value

--- nim.py
+++ nim.py
@@ -101,8 +101,12 @@ class NimAI():
         Return the Q-value for the state `state` and the action `action`.
         If no Q-value exists yet in `self.q`, return 0.
         """
-        raise NotImplementedError
-
+        state_tuple = (state[0], state[1], state[2], state[3])
+        if (state_tuple, action) in self.q:
+            return self.q[(state_tuple, action)]
+        else:
+            return 0
+        
     def update_q_value(self, state, action, old_q, reward, future_rewards):
         """
         Update the Q-value for the state `state` and the action `action`
